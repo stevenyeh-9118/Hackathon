fe655b643d2ddeb0ad74db53a1be3ea2a7cd0a7d
Yeh <stevenyeh89@gmail.com>
2025-10-21 22:18:42 +0800

    加入專案檔案。

---
+++ 01-ChangeNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _01_ChangeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入一個大於90的數:");
+            int max = int.Parse(Console.ReadLine());
+            if (max < 90)
+            {
+                Console.WriteLine("輸入必須至少大於或等於 90。");
+            }
+
+            for (int i = 1; i <= max; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("Dann");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Build");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("School");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
---
+++ 03/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入一句英文：");
+            string input = Console.ReadLine().ToLower();
+
+            string[] words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+                else
+                {
+                    wordCount[word] = 1;
+                }
+            }
+
+            Console.WriteLine();
+
+            foreach (var pair in wordCount)
+            {
+                Console.WriteLine($"{pair.Key} : {pair.Value}");
+            }
+        }
+    }
+}
